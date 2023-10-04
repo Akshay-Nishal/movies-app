e56712a620c408baafcf759794fce1c49634d961
Akshay <121626373+Akshay-Nishal@users.noreply.github.com>
2023-10-04 12:08:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies,setMovies]=useState([])
+  async function fetchMovies() {
+  const movieList = await(fetch('https://swapi.dev/api/films/')
+    .then(res=>{return( res.json() ) } )
+    .then(res=>{
+      return( res.results ) 
+    } )
+  )
+  console.log("Movie List : ",movieList)
+  setMovies(movieList)
+  }
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovies}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
