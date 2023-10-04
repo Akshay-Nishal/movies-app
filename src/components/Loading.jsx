afdafa5a5c4459f49cfbb74b7e6d79ebfebb6385
Akshay <121626373+Akshay-Nishal@users.noreply.github.com>
2023-10-04 16:04:55 +0530

    Fetch Movie on Page load

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,74 +1,90 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
-const ApiRetryWithCancel = () => {
-  const [retryCount, setRetryCount] = useState(0);
-  const [status, setStatus] = useState('Idle');
-  const [retrying, setRetrying] = useState(false);
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
+import './App.css';
 
-  const maxRetries = 10; // Maximum number of retry attempts
-  const retryDelay = 5000; // 5 seconds
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [retrying, setRetrying] = useState(false); // Add retrying state
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/film/');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      if (retrying) {
+        setError(error.message);
+        setTimeout(() => {
+          console.log("Fetching again in 5 sec");
+          fetchMoviesHandler();
+        }, 5000);
+      }
+    }
+    setIsLoading(false);
+  }, [retrying]);
 
   useEffect(() => {
-    let retryInterval;
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
-    const retryApi = () => {
-      setRetryCount(retryCount + 1);
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
 
-      fetch('YOUR_API_ENDPOINT')
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('API request failed');
-          }
-          return response.json();
-        })
-        .then((data) => {
-          // Handle the API response data here
-          setStatus('API request successful');
-          setRetrying(false);
-        })
-        .catch((error) => {
-          if (retryCount < maxRetries && retrying) {
-            setStatus(`Retry attempt ${retryCount} failed. Retrying in 5 seconds...`);
-            retryInterval = setTimeout(retryApi, retryDelay);
-          } else {
-            setStatus(`Maximum retry attempts reached.`);
-            setRetrying(false);
-          }
-        });
-    };
+  let content = <p>Found no movies.</p>;
 
-    if (retrying) {
-      retryApi();
-    }
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
 
-    return () => {
-      clearTimeout(retryInterval);
-    };
-  }, [retryCount, retrying]);
+  if (error) {
+    content = (
+      <div>
+        <p>{error}</p>
+        {retrying && (
+          <button onClick={() => setRetrying(false)}>Cancel Retry</button>
+        )}
+      </div>
+    );
+  }
 
-  const startApiRetry = () => {
-    setRetryCount(0);
-    setStatus('Retrying...');
-    setRetrying(true);
-  };
-
-  const cancelApiRetry = () => {
-    setRetrying(false);
-    setStatus('Retry canceled.');
-  };
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
-    <div>
-      <h1>API Retry with Cancel</h1>
-      <button onClick={startApiRetry} disabled={retrying}>
-        Start API Retry
-      </button>
-      <button onClick={cancelApiRetry} disabled={!retrying}>
-        Cancel Retry
-      </button>
-      <p>Status: {status}</p>
-    </div>
+    <React.Fragment>
+      {/* <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section> */}
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        {retrying || isLoading ? null : (
+          <button onClick={() => setRetrying(true)}>Retry</button>
+        )}
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
   );
-};
+}
 
-export default ApiRetryWithCancel;
+export default App;
